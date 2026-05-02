6d89a83384a8d01e3344b40508d023222b2ed806
Miras <mirasospan62@gmail.com>
2026-05-03 03:47:57 +0500

    updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
 import WelcomePage from "./pages/WelcomePage";
 import AuthPage from "./pages/Authpage";
 import StartupsPage from "./pages/StartupsPage";
@@ -6,10 +7,11 @@ import StartupDetailPage from "./pages/StartupDetailPage";
 import CreateStartupPage from "./pages/CreateStartupPage";
 import ProfilePage from "./pages/ProfilePage";
 import FavoritesPage from "./pages/FavoritesPage";
+
 import { getCurrentUser } from "./api/api";
 
 function PrivateRoute({ children }) {
-  return getCurrentUser() ? children : <Navigate to="/auth" />;
+  return getCurrentUser() ? children : <Navigate to="/auth" replace />;
 }
 
 export default function App() {
@@ -19,18 +21,36 @@ export default function App() {
         <Route path="/" element={<WelcomePage />} />
         <Route path="/auth" element={<AuthPage />} />
         <Route path="/startups" element={<StartupsPage />} />
-        <Route path="/startups/create" element={
-          <PrivateRoute><CreateStartupPage /></PrivateRoute>
-        } />
-        <Route path="/startups/:id" element={
-          <PrivateRoute><StartupDetailPage /></PrivateRoute>
-        } />
-        <Route path="/profile" element={
-          <PrivateRoute><ProfilePage /></PrivateRoute>
-        } />
-        <Route path="/favorites" element={
-          <PrivateRoute><FavoritesPage /></PrivateRoute>
-        } />
+        <Route path="/startups/:id" element={<StartupDetailPage />} />
+
+        <Route
+          path="/create"
+          element={
+            <PrivateRoute>
+              <CreateStartupPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route
+          path="/favorites"
+          element={
+            <PrivateRoute>
+              <FavoritesPage />
+            </PrivateRoute>
+          }
+        />
+
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
   );
